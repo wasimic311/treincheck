d3c7c8a11068a32ab7e286f7f6d78fdff78e011c
wasimic311 <wasim.ic311@gmail.com>
2026-07-19 03:06:13 +0200

    make poll interval configurable, guard entrypoint behind main

--- worker/__main__.py
+++ worker/__main__.py
@@ -1,18 +1,24 @@
 import logging
 import time
 
+from core.config import settings
 from worker.fetch_once import poll_once
-
-POLL_INTERVAL_SECONDS = 20
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 log = logging.getLogger("worker")
 
-while True:
-    try:
-        poll_once()
-        log.info("poll stored")
-    except Exception:
-        log.exception("poll failed, will retry next cycle")
-    time.sleep(POLL_INTERVAL_SECONDS)
+
+def main() -> None:
+    log.info("worker started, polling every %s seconds", settings.poll_interval_seconds)
+    while True:
+        try:
+            poll_once()
+            log.info("poll stored")
+        except Exception:
+            log.exception("poll failed, will retry next cycle")
+        time.sleep(settings.poll_interval_seconds)
+
+
+if __name__ == "__main__":
+    main()
 
