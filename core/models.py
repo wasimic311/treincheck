e9d13a796f072212e30a6b8a3243b85e0c964bc2
wasimic311 <wasim.ic311@gmail.com>
2026-07-22 20:32:34 +0200

    add Case and CaseTransition modles

--- core/models.py
+++ core/models.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.dialects.postgresql import JSONB
 from datetime import datetime
 from sqlalchemy import DateTime, func
+from sqlalchemy.orm import relationship
+from typing import List
+from sqlalchemy import ForeignKey
 
 class Base(DeclarativeBase):
     pass
@@ -16,3 +19,38 @@ class RawResponse(Base):
     status_code: Mapped[int]
     payload: Mapped[list | None] = mapped_column(JSONB)
 
+class Case(Base):
+    __tablename__ = "cases"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    ns_id: Mapped[str] = mapped_column(index=True)
+    ns_type: Mapped[str]
+    title: Mapped[str | None]
+
+    status: Mapped[str]
+    resolving_since: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    first_seen_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    last_seen_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    closed_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    ns_start: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    ns_end: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    stations_affected: Mapped[list | None] = mapped_column(JSONB)
+    impact: Mapped[int | None]
+    alternative: Mapped[str | None]
+
+    transitions: Mapped[List["CaseTransition"]] = relationship(back_populates="case")
+
+class CaseTransition(Base):
+    __tablename__ = "case_transitions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    from_status: Mapped[str]
+    to_status: Mapped[str]
+    observed_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    raw_response_id: Mapped[int] = mapped_column(ForeignKey("raw_responses.id"))
+
+    case_id: Mapped[int] = mapped_column(ForeignKey("cases.id"))
+    case: Mapped["Case"] = relationship(back_populates="transitions")
